f90db142af7f0424da50dc062b69c2dce17f25fc
Kenji_Takahashi <wozniakk@gmail.com>
2013-01-24 02:10:14 +0100

    packaged server

--- l4/server/setup.py
+++ l4/server/setup.py
@@ -1,1 +1,19 @@
 # -*- coding: utf-8 -*-
+
+
+from distutils.core import setup
+
+
+setup(
+    name="server",
+    version="0.1",
+    author="Karol Woźniak",
+    author_email="wozniakk@gmail.com",
+    packages=["server"],
+    scripts=["scripts/server", "scripts/controller"],
+    url="http://fake.url",
+    license="MIT",
+    description="A server for an strange game...",
+    long_description="N/A",
+    install_requires=["twisted"]
+)
